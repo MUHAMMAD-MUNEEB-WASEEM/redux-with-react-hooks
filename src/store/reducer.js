45f06fb54d91c8c5263706063f68b086c54f6b6d
Muhammad Muneeb Waseem <muneebwaseem78@gmail.com>
2020-11-27 11:09:36 +0500

    Practice redux using react hooks

---
+++ src/store/reducer.js
@@ -0,0 +1,28 @@
+// Var user = {name:”Muneeb”, email:”abc@gmail.com”}
+// Updating user;
+// Var newUser = {…user, name:”Zia Khan”, age:24}
+// So what we does is, we copy all the properties from the user, then update name, and add age property as well and store it in newUser.
+
+const initialState={
+    counter: 0
+}
+export const counterReducer = (state=initialState, action)=>{
+    switch(action.type){
+        case "INCREMENT":{
+            return {...state, counter: state.counter+1}
+            
+        }
+        case "DECREMENT":{
+            return {...state, counter: state.counter-1}
+        }
+        case "INCREMENT_BY_AMOUNT":{
+            return {
+                ...state, counter: state.counter+action.payload
+            }
+        }
+        default: {
+            return state
+        }
+        
+    }
+}
